217e5c1440c422b54be7a041a6a6132d07aa2a50
Beppe Karlsson <beppe@live.se>
2017-09-14 19:16:07 +0200

    image

--- src/Components/Collections/Image.js
+++ src/Components/Collections/Image.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+import * as imageActions from '../../actions/imageActions';
 
 import "./Image.css";
 
+const width = window.innerWidth > 400 ? 410: 290;
+
 class Image extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      fullscreen: false
-    };
+  toggleFullscreen = () => {
+    this
+      .props
+      .viewInFullscreen(this.props.img);
   }
 
   render() {
     let img = this.props.img;
+    let style = {
+      width: width
+    };
     return (
-      <div className={"list-image-container"}>
+      <div onClick={this.toggleFullscreen} className={"list-image-container"}>
         <img
           alt={img.value.title}
           src={img.value.path.smallURL}
-
-        />
+          style={style}/>
       </div>
     );
   }
 }
 
-export default Image;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    viewInFullscreen: (image) => {
+      dispatch(imageActions.viewInFullscreen(image, dispatch));
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Image);
