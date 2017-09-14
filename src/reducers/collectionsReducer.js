6dc19a244b2930529d1ff9e715433ae4db604565
Beppe Karlsson <beppe@live.se>
2017-09-14 19:13:17 +0200

    reducers

--- src/reducers/collectionsReducer.js
+++ src/reducers/collectionsReducer.js
@@ -7,27 +7,28 @@ const initialState = {
 
 export default function reducer(state=initialState, action) {
   switch (action.type) {
-    case "FETCH_COLLECTIONS_PENDING": {
+    case "FETCH_COLLECTIONS_PENDING": 
       return {
         ...state,
         fetching: true
       };
-    }
-    case "FETCH_COLLECTIONS_ERROR": {
+    
+    case "FETCH_COLLECTIONS_ERROR": 
       return {
         ...state,
         fetching: false,
         error: action.error
       };
-    }
-    case "RECEIVE_COLLECTIONS": {
+    
+    case "RECEIVE_COLLECTIONS": 
       return {
         ...state,
         collections: action.collections,
         fetched: true,
         fetching: false
       };
-    }
+    
+    default: 
+      return state;
   }
-  return state;
 };
--- src/reducers/imagesReducer.js
+++ src/reducers/imagesReducer.js
@@ -2,32 +2,47 @@ const initialState = {
   fetching: false,
   fetched: false,
   images: [],
-  error: null
+  error: null,
+  fullscreenImage: null
 };
 
 export default function reducer(state=initialState, action) {
   switch (action.type) {
-    case "FETCH_IMAGES_PENDING": {
+    case "FETCH_IMAGES_PENDING": 
       return {
         ...state,
         fetching: true
       };
-    }
-    case "FETCH_IMAGES_ERROR": {
+    
+    case "FETCH_IMAGES_ERROR": 
       return {
         ...state,
         fetching: false,
         error: action.error
       };
-    }
-    case "RECEIVE_IMAGES": {
+    
+    case "RECEIVE_IMAGES": 
       return {
         ...state,
         images: action.images,
         fetched: true,
         fetching: false
       };
-    }
+
+    case "VIEW_IN_FULLSCREEN":
+      return {
+        ...state,
+        fullscreenImage: action.image
+      };
+    
+    case "CLOSE_FULLSCREEN_VIEW":
+      return {
+        ...state,
+        fullscreenImage: null
+      }
+    
+    default: 
+      return state;
+    
   }
-  return state;
 };
