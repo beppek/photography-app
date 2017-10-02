e982118c456f6e8d58270d728d45001f02d3946d
Beppe Karlsson <beppe@live.se>
2017-10-02 17:59:35 +0200

    better fullscreen image

--- src/Components/Collections/FullscreenImage.js
+++ src/Components/Collections/FullscreenImage.js
@@ -7,6 +7,14 @@ import './FullscreenImage.css';
 
 class FullscreenImage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      showOverlay: false
+    }
+  }
+  
+
   closeFullscreen = () => {
     this.props.closeFullscreen();
   }
@@ -15,25 +23,48 @@ class FullscreenImage extends Component {
     this.props.closeFullscreen();
   }
 
+  showInfo = (e) => {
+    e.stopPropagation();
+    let {value} = this.props.img;
+    this.setState({showOverlay: !this.state.showOverlay});
+    console.log(value.description);
+  }
+
   render() {
     let {img} = this.props;
     let iconStyle = {
       color: "#CC0000"
     }
+
+    let overlayStatus = this.state.showOverlay ? "display" : "hidden";
     return (
-      <div onClick={this.clickOutside} className="fullscreen-image-container">
-        <div className="fullscreen-image-topbar">
-          <h2 className="image-title">{img.value.title}</h2>
-          <IconButton 
-            onClick={this.closeFullscreen}
-            iconStyle={iconStyle} 
-            className="close-button" 
-            iconClassName="fa fa-times"
-          />
+      <div onClick={this.clickOutside} className="fullscreen-image">
+          <div className="fullscreen-image-topbar">
+            <h2 className="image-title">{img.value.title}</h2>
+            <IconButton 
+              onClick={this.closeFullscreen}
+              iconStyle={iconStyle} 
+              className="close-button" 
+              iconClassName="fa fa-times"
+            />
+          </div>
+        <div className="fullscreen-image-background">
+          <div className="fullscreen-image-container">
+            <img 
+              src={img.value.path.downloadURL}
+            />
+            <IconButton 
+              onClick={this.showInfo}
+              iconStyle={iconStyle} 
+              className="info-button" 
+              iconClassName="fa fa-info"
+            />
+            
+            <div className={`fullscreen-image-overlay fullscreen-image-overlay-${overlayStatus}`}>
+              <p>{img.value.description}</p>
+            </div>
+          </div>
         </div>
-        <img 
-          src={img.value.path.downloadURL}
-        />
       </div>
     );
   }
