f25dc11986171b62237d44ee2ed868caf16a5f31
Beppe Karlsson <beppe@live.se>
2017-10-02 18:00:49 +0200

    fix masonry grid

--- src/Components/MasonryGrid/MasonryGrid.js
+++ src/Components/MasonryGrid/MasonryGrid.js
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./MasonryGrid.css";
 
-const width = window.innerWidth > 400 ? 420: 300;
+const width = window.innerWidth > 400
+  ? 380
+  : 300;
 
 class MasonryGrid extends Component {
   constructor(props) {
     super(props);
-    this.state = { width: "0", height: "0" };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.state = {
+      width: "0",
+      height: "0"
+    };
+    this.updateWindowDimensions = this
+      .updateWindowDimensions
+      .bind(this);
   }
 
   componentDidMount() {
@@ -20,42 +27,78 @@ class MasonryGrid extends Component {
   }
 
   updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    this.setState({width: window.innerWidth, height: window.innerHeight});
   }
 
-  calculateRows = () => {
-    let rows;
-    rows = Math.floor(this.state.width / width);
-    return rows;
+  calculateCols = () => {
+    let cols;
+    cols = Math.floor(this.state.width / width);
+    return cols;
   };
 
-  assignImgsToRows() {
-    let numOfRows = this.calculateRows();
-    let rows = [];
-    let index;
-    for (index = 0; index < numOfRows; index += 1) {
-      rows.push({ key: index, imgs: [] });
+  /**
+   * Assigns images to cols in a masonry fashion, evenly distributed over the columns
+   */
+  assignImgsToCols() {
+    let numOfCols = this.calculateCols();
+    
+    // Init cols
+    let cols = [];
+    let i;
+    for (i = 0; i < numOfCols; i += 1) {
+      cols.push({key: i, imgs: []});
     }
+
     let {imgs} = this.props;
-    let rowLength = imgs.length / numOfRows;
-    let currentRow = 0;
-    if (rows.length > 0) {
-      imgs.map((img, i) => {
-        rows[currentRow].imgs.push(img);
-        if (i + 1 - currentRow * rowLength >= rowLength) {
-          currentRow += 1;
-        }
+    
+    // Init colHeights
+    let colHeights = [];
+    for (i = 0; i < cols.length; i+=1) {
+      colHeights[i] = 0;
+    };
+
+    // Add image to shortest column
+    if (cols.length > 0) {
+      imgs.forEach((img) => {
+        let col = this.findShortestColumn(colHeights);
+        cols[col].imgs.push(img);
+        let {dimensions} = img.props.img.value;
+        colHeights[col] += dimensions.small.maxHeight;
       });
     }
-    return rows;
+    
+    return cols;
+  }
+
+  /**
+   * Finds the shortest column in the grid
+   * @param {Array} cols
+   * @return {int} col - shortest column 
+   */
+  findShortestColumn(cols) {
+    let min = cols[0];
+    let index = 0;
+    let i;
+    for (i = 0; i < cols.length; i+=1) {
+      if (cols[i] < min) {
+        min = cols[i];
+        index = i;
+      }
+    }
+    return index;
   }
 
   render() {
-    let rows = this.assignImgsToRows();
+    let rows = this.assignImgsToCols();
     let grid = [];
     rows.forEach(row => {
       grid.push(
-        <div key={row.key} style={{width: width}} className={`grid-row row-${row.key}`}>
+        <div
+          key={row.key}
+          style={{
+          width: width
+        }}
+          className={`grid-row row-${row.key}`}>
           {row.imgs}
         </div>
       );
